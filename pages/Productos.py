ed6323d4e4fa41b933444b6f14a69e62d86d8872
SergioSKA27 <sergioska81@hotmail.com>
2024-02-02 05:57:59 -0600

    Agrega opciones de búsqueda y agregar en la página de productos

--- pages/Productos.py
+++ pages/Productos.py
@@ -18,6 +18,12 @@ if 'option' not in st.session_state:
     st.session_state.option = 'None'
 
 
+if 'gender' not in st.session_state:
+    st.session_state.gender = 'Hombre'
+
+if 'talla' not in st.session_state:
+    st.session_state.talla = 26
+
 # Invetory Dashboard
 
 with elements('header'):
@@ -46,7 +52,48 @@ with elements('header'):
         mui.Button(mui.icon.Add(),mui.Typography('Agregar',variant='caption',sx={'margin': '10px','fontSize': '2vw'}),color='secondary',onClick=handle_add)
         mui.Button(mui.icon.DeleteForever(),mui.Typography('Eliminar',variant='caption',sx={'margin': '10px','fontSize': '2vw'}),color='secondary',onClick=handle_delete)
 
-    if st.session_state.option == 'search':
+if st.session_state.option == 'search':
+    with elements('search'):
         with mui.Box(sx={'display': 'flex', 'flexDirection': 'row','alignItems': 'center'}):
             mui.icon.Search()
             mui.TextField(label='Buscar',variant='outlined',sx={'margin': '10px','fontSize': '2vw','width': '100%'})
+elif st.session_state.option == 'add':
+    with elements('add'):
+        with mui.Box(sx={'display': 'flex', 'flexDirection': 'row','alignItems': 'center'}):
+            mui.icon.Abc()
+            mui.TextField(label='Clave',variant='outlined',sx={'margin': '10px','fontSize': '2vw','width': '100%'})
+            mui.icon.Ballot()
+            mui.TextField(label='Modelo',variant='outlined',sx={'margin': '10px','fontSize': '2vw','width': '100%'})
+        with mui.Box(sx={'display': 'flex', 'flexDirection': 'row','alignItems': 'center'}):
+            mui.icon.Female()
+            with mui.FormControl(sx={"width":"100%", "margin": "10px"}):
+                mui.InputLabel("Genero",id="genero")
+                with mui.Select(value=st.session_state.gender.props.value if st.session_state.gender != "Hombre" else "Hombre"
+                ,labelId="genero", id="gender", label="Genero", sx={"width":"100%"}, onChange=sync(None,'gender')) :
+                    mui.MenuItem("Hombre", value="H")
+                    mui.MenuItem("Mujer", value="M")
+            mui.icon.ContentCut()
+            mui.TextField(label='Corte',variant='outlined',sx={'margin': '10px','fontSize': '2vw','width': '100%'})
+
+        with mui.Box(sx={'display': 'flex', 'flexDirection': 'row','alignItems': 'center'}):
+            mui.icon.Straighten()
+            with mui.FormControl(sx={"width":"100%", "margin": "10px"}):
+                mui.InputLabel("Talla",id="talla")
+                with mui.Select(value=st.session_state.talla.props.value if st.session_state.talla != 26 else 26,
+                labelId="talla", id="talla", label="Talla", sx={"width":"100%"}, onChange=sync(None,'talla')) :
+                    mui.MenuItem("26", value=26)
+                    mui.MenuItem("28", value=28)
+                    mui.MenuItem("30", value=30)
+                    mui.MenuItem("32", value=32)
+                    mui.MenuItem("34", value=34)
+                    mui.MenuItem("36", value=36)
+            mui.icon.Inventory()
+            mui.TextField(label='Existencia',type='number',variant='outlined',sx={'margin': '10px','fontSize': '2vw','width': '100%'})
+
+        with mui.Box(sx={'display': 'flex', 'flexDirection': 'row','alignItems': 'center'}):
+            mui.icon.Paid()
+            mui.TextField(label='Precio',type='number',variant='outlined',sx={'margin': '10px','fontSize': '2vw','width': '100%'})
+
+        with mui.Box(sx={ '& > :not(style)': { 'm': 1 }, 'display': 'flex', 'flexDirection': 'row', 'justifyContent': 'flex-end', 'alignItems': 'right'}):
+            with mui.Fab(color="primary", aria_label="add"):
+                mui.icon.Save()
