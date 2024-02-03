c7d2d8c914febb05cd36644a4d62898bc21bd17f
SergioSKA27 <sergioska81@hotmail.com>
2024-02-03 05:13:19 -0600

    Actualizar configuración de página en Productos.py

--- pages/Productos.py
+++ pages/Productos.py
@@ -6,7 +6,7 @@ import asyncio
 import requests
 import base64
 
-st.set_page_config(page_title='Inventario',page_icon=' 🩱 ',layout='wide',initial_sidebar_state='collapsed')
+st.set_page_config(page_title='Inventario',page_icon='🩱',layout='wide',initial_sidebar_state='collapsed')
 # Create a connection to the XataDB
 xata = st.connection('xata',type=XataConnection)
 
