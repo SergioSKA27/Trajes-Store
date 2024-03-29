2d2e87b11c55d8da31d699ee44ade9535f58e037
SergioSKA27 <sergioska81@hotmail.com>
2024-03-28 21:38:26 -0600

    Agrega estilos de CSS y enlaces de navegación a la página de productos simples

--- pages/simpleProducts.py
+++ pages/simpleProducts.py
@@ -7,6 +7,21 @@ st.set_page_config(page_title='Inventario',page_icon='🩱',layout='wide')
 xata = st.connection('xata',type=XataConnection)
 client =XataClient(api_key=st.secrets['XATA_API_KEY'],db_url=st.secrets['XATA_DB_URL'])
 
+st.markdown("""
+<style>
+#MainMenu, header, footer {visibility: hidden;}
+.appview-container .main .block-container
+{
+    padding-top: 0.5px;
+    padding-left: 1rem;
+    padding-right: 1rem;
+    padding-bottom: 0.5rem;
+}
+</style>
+""",unsafe_allow_html=True)
+
+
+
 def validate_product(clave,modelo,corte,existencia,precio):
     if clave == '':
         st.error('La clave del producto no puede estar vacia')
@@ -31,8 +46,12 @@ def validate_product(clave,modelo,corte,existencia,precio):
 if 'upload' not in st.session_state:
     st.session_state.upload = False
 
+navcols = st.columns([0.4,0.2,0.2,0.1,0.1])
+navcols[0].title('➕Agregar Producto')
+navcols[2].page_link('pages/ProductosMain.py',label='Gestion de Productos',icon='🩱',help='Modifica, elimina y busca productos en el inventario',use_container_width=True)
+navcols[3].page_link('pages/Ventas.py',label='Ventas',icon='💰',help='Registra las ventas de productos en la tienda',use_container_width=True)
+navcols[4].page_link('pages/pedidos.py',label='Pedidos',icon='🚚',help='Revise los pedidos pendientes y entregados de la tienda',use_container_width=True)
 
-st.title('Agregar Producto',anchor=False)
 st.divider()
 
 clave = st.text_input('Clave',placeholder='Clave del producto',help='La clave del producto es unica')
